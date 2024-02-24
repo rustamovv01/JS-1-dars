fd863a7df83a3dd65b3c6df6d3a9f42f2bca7b8a
Mukhammadaziz <rustamov977075073@gmail.com>
2024-02-24 14:39:04 +0500

    1-dars

---
+++ main.js
@@ -0,0 +1,47 @@
+// console.clear()
+// console.time()
+// console.log('Hello World!')
+// console.warn('ogohlantlantirish')
+// console.error('xatolik')
+// console.info('habar berish')
+// console.timeEnd()
+
+// var son = 45
+// console.log(son)
+
+// var son = 90;
+// var son = 45;
+
+// let raqam = 56;
+
+// let raqam1 = 100;
+
+// const pi = 3.14;
+// {
+//     var a = 300;
+// }
+// console.log(a)
+
+// var ism = prompt('ism kiriting:');
+// let yosh = prompt('yoshingini kitiring:')
+
+// console.log('ism: '+ ism)
+// console.log('yosh: ' + yosh)
+
+var ism = prompt('Ismingizni kiriting: ');
+console.log('ism: '+ ism);
+
+var familya = prompt('Familyangizni kiriting: ');
+console.log('familya: '+ familya);
+
+var yosh = prompt('Yoshingizni kiriting: ');
+console.log('yosh: '+ yosh);
+
+var hobby = prompt('Yoqtirgan faningizni kiriting: ');
+console.log('Yoqtirgan faningiz: '+ hobby);
+
+var tuman = prompt('Yashaydigan tumaningizni kiriting: ');
+console.log('Yashaydigan tumaningiz: '+ tuman);
+
+
+alert("malutmolaringizni kiritganiz uchun tashakkur")
